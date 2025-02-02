1fb666b6489c76100fae9afbe7205134dee4ebcb
heet <heetgala99@gmail.com>
2025-02-02 22:34:57 +0530

    vite mock plugin for local frontend mock data

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteMockServe } from 'vite-plugin-mock';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  plugins: [
+    react(),
+    viteMockServe({
+      mockPath: 'mock',
+      localEnabled: true,
+    })
+  ],
+  css: {
+    postcss: './postcss.config.js',
+  },
   resolve: {
     alias: {
       '@': '/src',
