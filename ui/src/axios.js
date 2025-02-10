d54db6cbc79e1aba182d9f6ecefb43caedfe28ad
heet <heetgala99@gmail.com>
2025-02-10 14:00:48 +0530

    adding RTK and axios interceptors

--- ui/src/axios.js
+++ ui/src/axios.js
@@ -8,6 +8,7 @@ const axiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
   timeout: 10000,
 });
 
@@ -16,7 +17,7 @@ axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = token;
     }
     return config;
   },
